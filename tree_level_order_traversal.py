2d25683e78990434b30669760133480f0ecac49a
Erick Panni <erickpanni@icloud.com>
2022-09-05 20:58:45 -0300

    Tree: Level Order Traversal

---
+++ tree_level_order_traversal.py
@@ -0,0 +1,73 @@
+"""
+Hackerrank: Tree: Level Order Traversal
+
+Given a pointer to the root of a binary tree, you need to print the level order traversal of this tree. In level-order traversal, nodes are visited level by level from left to right. Complete the function  and print the values in a single line separated by a space.
+
+For example:
+
+     1
+      \
+       2
+        \
+         5
+        /  \
+       3    6
+        \
+         4  
+For the above tree, the level order traversal is .
+
+Input Format
+
+You are given a function,
+
+void levelOrder(Node * root) {
+
+}
+Constraints
+
+ Nodes in the tree  
+
+Output Format
+
+Print the values in a single line separated by a space.
+
+Sample Input
+
+     1
+      \
+       2
+        \
+         5
+        /  \
+       3    6
+        \
+         4  
+Sample Output
+
+1 2 5 3 6 4
+
+Explanation
+
+We need to print the nodes level by level. We process each level from left to right.
+Level Order Traversal: .
+"""
+
+
+#Solution:
+    
+    """
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    queue=[root]
+    output=[]
+    while queue:
+        current=queue.pop(0)
+        print(current.info, end=' ')
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
