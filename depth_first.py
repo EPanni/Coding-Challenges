00ff21430e2bf51fd94d6d873a0729a51e6dcf7b
Erick Panni <erickpanni@icloud.com>
2022-09-05 13:00:44 -0300

    Depth First Algoritm - Recursively

--- depth_first.py
+++ depth_first.py
@@ -1,18 +1,24 @@
-""" Depth First Algorithm in Python"""
+""" Depth First Algorithm in Python [Recursively] """
+
+graph = {"a": ["b", "c"], "b": ["d"], "c": ["e"], "d": ["f"], "e": [], "f": []}
+
+# def depth_first_print(graph, source_node: str):
+#    """Main Function"""
+#    stack = [source_node]
+#
+#    while stack:
+#        current = stack.pop()
+#        print(current)
+#
+#        for neighbor in graph[current]:
+#            stack.append(neighbor)
 
 
 def depth_first_print(graph, source_node: str):
-    """Main Function"""
-    stack = [source_node]
+    """Recursively implementation"""
+    print(source_node)
+    for neighbor in graph[source_node]:
+        depth_first_print(graph, neighbor)
 
-    while stack:
-        current = stack.pop()
-        print(current)
-
-        for neighbor in graph[current]:
-            stack.append(neighbor)
-
-
-graph = {"a": ["b", "c"], "b": ["d"], "c": ["e"], "d": ["f"], "e": [], "f": []}
 
 depth_first_print(graph, "a")
